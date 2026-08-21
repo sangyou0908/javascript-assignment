4078415323015f1ef302a72a8c64999a0a660aaa
“sangyou0908” <sangyou0908@gmail.com>
2026-08-21 15:55:56 +0900

    feat: 영화 데이터 변수 생성

--- day01/script.js
+++ day01/script.js
@@ -1,24 +1,6 @@
-// 수업 중 실습 1
-
-// console.log("Hello!");
-// console.log("JavaScript 처음 시작");
-// console.log("12345");
-
-
-// 수업 중 실습 2
-
-// const serviceName = "AI Store";
-// let productPrice = 39000;
-// const isLoggedIn = true;
-
-// console.log(serviceName);
-// console.log(productPrice);
-// console.log(isLoggedIn);
-
-// let cartCount = 2;
-// console.log(cartCount);
-
-// cartCount = 3;
-// console.log(cartCount);
-
-
+const title = "인터스텔라";
+const voteAverage = 8.7;
+const voteCount = 32000;
+const popularity = 150.5;
+const releaseDate = "2014-11-05";
+const originalLanguage = "en";
