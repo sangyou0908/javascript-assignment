9abf3deb43958f9fb9deaab49ee22112e4bd66f2
“sangyou0908” <sangyou0908@gmail.com>
2026-08-21 15:57:31 +0900

    feat: 영화 데이터 Console 출력

--- day01/script.js
+++ day01/script.js
@@ -4,3 +4,10 @@ const voteCount = 32000;
 const popularity = 150.5;
 const releaseDate = "2014-11-05";
 const originalLanguage = "en";
+
+console.log(title);
+console.log(voteAverage);
+console.log(voteCount);
+console.log(popularity);
+console.log(releaseDate);
+console.log(originalLanguage);
