c082898eef8c9437f570964c5893ed24d1eb295d
“sangyou0908” <sangyou0908@gmail.com>
2026-08-21 15:38:55 +0900

    refactor: 폴더 구조 정리

---
+++ day01/script.js
@@ -0,0 +1,24 @@
+// 수업 중 실습 1
+
+// console.log("Hello!");
+// console.log("JavaScript 처음 시작");
+// console.log("12345");
+
+
+// 수업 중 실습 2
+
+// const serviceName = "AI Store";
+// let productPrice = 39000;
+// const isLoggedIn = true;
+
+// console.log(serviceName);
+// console.log(productPrice);
+// console.log(isLoggedIn);
+
+// let cartCount = 2;
+// console.log(cartCount);
+
+// cartCount = 3;
+// console.log(cartCount);
+
+
