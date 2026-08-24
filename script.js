f58fe63395d3f20d2ac5a724e18cb6cba0a2e383
“sangyou0908” <sangyou0908@gmail.com>
2026-08-24 17:11:57 +0900

    feat: 영화 3편 배열 추가

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ if (movie.voteAverage >= 8 && movie.voteCount >= 30000) {
 
 const favoriteButtonText = movie.isFavorite ? "찜 해제" : "찜하기";
 console.log(favoriteButtonText);
+
+const movies = [
+  {
+    id: 1,
+    title: "인셉션",
+    voteAverage: 8.4,
+    releaseDate: "2010-07-15",
+  },
+  {
+    id: 2,
+    title: "인터스텔라",
+    voteAverage: 8.7,
+    releaseDate: "2014-11-05",
+  },
+  {
+    id: 3,
+    title: "테넷",
+    voteAverage: 7.3,
+    releaseDate: "2020-08-26",
+  },
+];
