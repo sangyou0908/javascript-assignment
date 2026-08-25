2e5d9b6d801aa643afbb268ea09f2e669a87a780
“sangyou0908” <sangyou0908@gmail.com>
2026-08-25 19:01:33 +0900

    feat: 영화 한줄 소개 카드2 추가

--- day03/script.js
+++ day03/script.js
@@ -11,8 +11,13 @@ const description = document.querySelector(".description");
 description.classList.add("text-primary", "fw-bold");
 
 const movieList = document.querySelector("#movie-list");
+
 const movieMessageCard = document.createElement("div");
-
 movieMessageCard.textContent = getMovieMessage("인셉션", 8.4);
-
+movieMessageCard.classList.add("border", "rounded", "p-3", "mb-2");
 movieList.append(movieMessageCard);
+
+const movieMessageCard2 = document.createElement("div");
+movieMessageCard2.textContent = getMovieMessage("인터스텔라", 8.7);
+movieMessageCard2.classList.add("border", "rounded", "p-3", "mb-2");
+movieList.append(movieMessageCard2);
