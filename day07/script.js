a2c8688d464b03264294c701277be3e878e02295
“sangyou0908” <sangyou0908@gmail.com>
2026-08-31 15:38:50 +0900

    feat: 기본 파일 세팅 및 movie 데이터 추가

---
+++ day07/script.js
@@ -0,0 +1,11 @@
+const movie = {
+  id: 550,
+  title: "Fight Club",
+  vote_average: 8.4,
+  vote_count: 0,
+  detail: {
+    runtime: 139,
+  },
+};
+
+const genres = ["Drama", "Thriller", "Crime"];
