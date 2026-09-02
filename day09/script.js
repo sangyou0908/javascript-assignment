3c757123922da9ffcc9ffeb20002d7cfd2db1c23
“sangyou0908” <sangyou0908@gmail.com>
2026-09-02 23:15:42 +0900

    feat: 영화 객체에서 특정 속성 가져오기

--- day09/script.js
+++ day09/script.js
@@ -11,14 +11,12 @@ const container = document.querySelector("#movie-list");
 
 function createMovieCard(movie) {
   // TODO 1. title, vote_average, poster_path를 구조 분해 할당으로 가져오세요.
+  const { title, vote_average, poster_path } = movie;
 
   // TODO 2. movie-card 클래스를 가진 div 요소를 만드세요.
-
   // TODO 3. 포스터 img 요소를 만들고 src와 alt를 설정하세요.
   // poster_path가 없으면 대체 이미지를 사용하세요.
-
   // TODO 4. 영화 제목 h3 요소와 평점 p 요소를 만드세요.
-
   // TODO 5. 만든 요소를 card에 추가하고 card를 반환하세요.
 }
 
