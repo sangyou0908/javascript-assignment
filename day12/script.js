8d980b465e13d8f9ffb7efb7d6704b6812fc7ea9
“sangyou0908” <sangyou0908@gmail.com>
2026-09-07 17:28:27 +0900

    feat: localStorage에 최근 검색어 배열 저장하기

--- day12/script.js
+++ day12/script.js
@@ -120,6 +120,7 @@ async function searchMovies(keyword) {
     keywords.unshift(keyword);
 
     // TODO 06. 최근 검색어 배열 localStorage에 저장하기
+    localStorage.setItem("keywords", JSON.stringify(keywords));
 
     // TODO 07. 최근 검색어 목록 다시 그리기
 
